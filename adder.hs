3cd4f343a97bf690d24a849475eba5f7ad5c17ee
Colin Woodbury <colingw@gmail.com>
2012-04-08 14:13:28 +0900

    Added support for Doubles
    - Used regexes to determine what a properly normatted number is.
    - Was also able to factor out the `addUp` function.

--- adder.hs
+++ adder.hs
@@ -1,25 +1,20 @@
 -- adder.hs
 -- Adds all the numbers in a given file, formatted one number per line.
--- Comments are allowed and  are removed in `getNums'.
+-- Comments are allowed and  are removed in `parseNums`.
 -- In truth, any line that doesn't start with a number is ignored.
--- BUG: Doesn't support Doubles. Need to regex the numbers! i.e. xxx.xxx
 
 import System.Environment (getArgs)
-import Data.Char (ord, isDigit)
+import Text.Regex.Posix ((=~))
 
 main = do
-  (filename:_) <- getArgs
-  nums <- getNums filename
-  print $ addUp nums
+  (fname:_) <- getArgs
+  numbers   <- getNums fname
+  print . sum . map readToDouble $ numbers
+    where readToDouble n = read n :: Double
 
 getNums :: String -> IO [String]
-getNums filename = readFile filename >>= return . parseNums
+getNums fname = readFile fname >>= return . parseNums
       
 parseNums :: String -> [String]
 parseNums = filter goodNum . map (head . words) . filter (/= "") . lines
-    where goodNum = and . map isDigit
-
-addUp :: (Num a, Read a) => [String] -> a
-addUp = foldl (\acc num -> acc + read num) 0 
-
-   
+    where goodNum num = num =~ "^[0-9]+[.]?[0-9]+$" :: Bool
